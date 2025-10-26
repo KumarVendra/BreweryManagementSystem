15a09df18d32acd0f58f272b2a5fa23bcda016e0
KumarVendra <vendrakumar45@gmail.com>
2025-10-26 20:38:19 +0530

    npm commit 4

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
