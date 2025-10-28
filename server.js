704a60ccb7f647ca29bf6db2e9d395a8edeff1ea
KumarVendra <vendrakumar45@gmail.com>
2025-10-28 12:01:42 +0530

    server.js commit

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/cafeDB")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String },
+  description: { type: String },
+  price: { type: Number, required: true, min: 0 },
+  stock: { type: Number, default: 0, min: 0 },
+  size: { type: String }
+});
