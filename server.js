387fe668f53460a5a94d994801672aac16fcf60f
KumarVendra <vendrakumar45@gmail.com>
2025-11-04 06:35:48 +0000

    server commits3

--- server.js
+++ server.js
@@ -42,3 +42,13 @@ app.post("/api/products", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find().sort({ name: 1 });
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
