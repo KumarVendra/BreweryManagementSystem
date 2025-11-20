4bc66bf8cc4c53e5a7d73559aa85bef2d2a273dd
KumarVendra <vendrakumar45@gmail.com>
2025-11-20 19:34:22 +0000

    server test commit

--- server.test.js
+++ server.test.js
@@ -1,3 +1,9 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const { app, Product } = require('../server');
+beforeAll(async () => {
+  await mongoose.connect('mongodb://127.0.0.1:27017/cafeTestDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
